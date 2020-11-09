2d8d05629f70391aa40f2dd632dc09dfae6ec7f2
ssheaks <sarah.sheaks@gmail.com>
2020-11-09 13:53:35 -0800

    event handler arrow function

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ switchNameHandler = (newName) => {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button onClick={this.switchNameHandler.bind(this, 'Haras')}>Switch Name</button>
+        <button onClick={() => this.switchNameHandler('Haras')}>Switch Name</button>
         <Person 
         name={this.state.persons[0].name} 
         age={this.state.persons[0].age}/>
