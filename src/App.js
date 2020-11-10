9671a16ee97910f687ce66a2b4f5221a10e304b7
ssheaks <sarah.sheaks@gmail.com>
2020-11-09 16:09:35 -0800

    add syling

--- src/App.js
+++ src/App.js
@@ -26,19 +26,29 @@ state = {
   nameChangeHandler = (event) => {
     this.setState( {
       persons: [
-        {name: 'Haras', age: 63},
+        {name: this.state.persons[0].name, age: 63},
         {name: event.target.value, age: 50},
-        {name: 'Ylloh', age: 90}
+        {name: this.state.persons[2].name, age: 90}
       ]
     })
   }
 
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button onClick={() => this.switchNameHandler('Haras')}>Switch Name</button>
+        <button 
+        style={style}
+        onClick={() => this.switchNameHandler('Haras')}>Switch Name</button>
         <Person 
         name={this.state.persons[0].name} 
         age={this.state.persons[0].age}/>
