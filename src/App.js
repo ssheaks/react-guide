ce74b7d40c7af989e8719b70cae4f517b8563490
ssheaks <sarah.sheaks@gmail.com>
2020-11-12 17:44:53 -0800

    update with immutable state and dynamic lists

--- src/App.js
+++ src/App.js
@@ -5,36 +5,55 @@ import Person from './Person/Person'
 class App extends Component {
 state = {
   persons: [
-    {name: 'Sarah', age: 36},
-    {name: 'Gazelle', age: 5},
-    {name: 'Holly', age: 9}
+    {id: 'fvnk', name: 'Sarah', age: 36},
+    {id: 'v35j', name: 'Gazelle', age: 5},
+    {id: 'ne98', name: 'Holly', age: 9}
   ],
   otherState: 'some other value',
   showPersons: false
 }
 
-  switchNameHandler = (newName) => {
-    //Change the state with React's special method in the React extended {Component} object: setState(). This ensures React updates the state and the DOM
-    this.setState({
-      persons: [
-        {name: newName, age: 63},
-        {name: 'Ellezeg', age: 50},
-        {name: 'Ylloh', age: 90}
-      ]
-    })
-    console.log(this.state)
-  }
+  // switchNameHandler = (newName) => {
+  //   //Change the state with React's special method in the React extended {Component} object: setState(). This ensures React updates the state and the DOM
+  //   this.setState({
+  //     persons: [
+  //       {name: newName, age: 63},
+  //       {name: 'Ellezeg', age: 50},
+  //       {name: 'Ylloh', age: 90}
+  //     ]
+  //   })
+  //   console.log(this.state)
+  // }
 
-  nameChangeHandler = (event) => {
+
+  nameChangeHandler = (event, id) => {
+    //use findIndex() method to check if the id is equal to the id of the person and if true return the index of the person we are updating.
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id ===id;
+    })
+
+    //Using the index returned above we can set a constant person to update the state with the input value. Make sure not to update the object in the state directly, use spread operator or Object.Assign:
+    //const person = Object.assign({}, this.state.persons[personIndex]);
+    const person = {...this.state.persons[personIndex]};
+
+    person.name = event.target.value;
+
+    //work with copy of persons array to 
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    //set the state to the updated persons array
     this.setState( {
-      persons: [
-        {name: this.state.persons[0].name, age: 63},
-        {name: event.target.value, age: 50},
-        {name: this.state.persons[2].name, age: 90}
-      ]
+      persons: persons
     })
   }
 
+  deletePersonHandler = (personsIndex) => {
+    // const persons = this.state.slice();
+    const persons = [...this.state.persons];
+    persons.splice(personsIndex, 1);
+    this.setState({persons: persons});
+  }
   //use the arrow function whereby we assign an arrow function rather than just creating a function (togglePersonsHandler()) because, due to the nature of javascript, it will assure that the 'this' reference return to the class to execute this method.
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
@@ -50,27 +69,31 @@ state = {
       cursor: 'pointer'
     }
 
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+          <div>
+            {this.state.persons.map((person, index) => {
+              return <Person 
+              click={() => this.deletePersonHandler(index)}
+              name={person.name}
+              age={person.age}
+              key={person.id}
+              changed={(event) => this.nameChangeHandler(event, person.id)} />
+            })}
+        </div>
+      )
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
         <button 
         style={style}
-        onClick={this.togglePersonsHandler}>Switch Name</button>
-        {this.state.showPersons === true ?
-        <div>
-          <Person 
-          name={this.state.persons[0].name} 
-          age={this.state.persons[0].age}/>
-          <Person 
-          name={this.state.persons[1].name} 
-          age ={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, 'Sarah')}
-          changed={this.nameChangeHandler}>My Hobbies: Eating Poo</Person>
-          <Person 
-          name={this.state.persons[2].name} 
-          age={this.state.persons[2].age}/>
-        </div> : null}
+        onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        {persons}
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App'))
