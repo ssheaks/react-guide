074320845c5b56e2509a8759a45eda39319c440b
ssheaks <sarah.sheaks@gmail.com>
2020-11-19 11:33:21 -0800

    update button to use Styled Components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import './App.css';
-import Radium, {StyleRoot} from 'radium';
 import Person from './Person/Person'
+
+const StyledButton = styled.button`
+  background-color: green;
+  color: white;
+  font: inherit;
+  border: 1px solid blue;
+  padding: 8px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: lightgreen;
+    color: black;
+  `;
 
 class App extends Component {
 state = {
@@ -109,19 +122,20 @@ state = {
     }
 
     return (
-      <StyleRoot>
         <div className="App">
           <h1>Hi, I'm a React App</h1>
           <p className={classes.join(' ')}>This is really working!</p>
-          <button 
-          style={style}
-          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+          {/* <button 
+          style={style} */}
+          <StyledButton onClick={this.togglePersonsHandler}>
+          Toggle Persons
+          {/* onClick={this.togglePersonsHandler}>Toggle Persons</button> */}
+          </StyledButton>
           {persons}
         </div>
-      </StyleRoot>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App'))
   }
 }
 
-export default Radium(App);
+export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,19 @@
 import React from 'react';
-import Radium from 'radium';
+import styled from 'styled-components';
 
-import './Person.css'
+// import './Person.css'
+
+const StyledDiv = styled.div`
+    width: 60%;
+    margin: 16px auto;
+    border: 1px solid #eeeeee;
+    box-shadow: 0 2px 3px #cccccc;
+    padding: 16px;
+    text-align: center;
+
+    @media (min-width: 500px) {
+            width: 450px;
+    }`;
 
 const person = (props) => {
     console.log(props)
@@ -11,12 +23,13 @@ const person = (props) => {
         }
     };
     return (
-    <div className='Person' style={style}>
-        <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
-        <p>{props.children}</p>
-        <input type='text' onChange={props.changed} value={props.name} />
-    </div>
+    // <div className='Person' style={style}>
+        <StyledDiv>
+            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
+            <p>{props.children}</p>
+            <input type='text' onChange={props.changed} value={props.name} />
+        </StyledDiv>
     );
 }
 
-export default Radium(person);
+export default person;
