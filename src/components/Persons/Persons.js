c81fcc5f405f008bcb91ad183d55e736eed7aeff
ssheaks <sarah.sheaks@gmail.com>
2020-12-14 13:46:03 -0800

    update with lifecycle hooks

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -2,14 +2,17 @@ import React from 'react';
 import Person from './Person/Person'
 
 const persons = (props) => {
-    console.log('[Persons.js] rendering...');
+    
     return props.persons.map((person, index) => {
-    return <Person 
-        click={() => props.clicked(index)}
-        key={person.id}
-        name={person.name}
-        age={person.age}
-        changed={(event) => props.changed(event, person.id)} />
+        console.log('[Persons.js] rendering...');
+        return (
+            <Person 
+            click={() => props.clicked(index)}
+            key={person.id}
+            name={person.name}
+            age={person.age}
+            changed={(event) => props.changed(event, person.id)} />
+        )
     });
 };
 
